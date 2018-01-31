c8f32b9a1276d5f2ce496620ee6c11447643aa06
QIAN Weishuo <qianws@fenbi.com>
2018-01-31 14:46:47 +0800

    MOD: reading gen_notebook to understand the data preprocess pipeline, and make it work on my Mac

--- scripts/gen_train/gen_notebook.py
+++ scripts/gen_train/gen_notebook.py
@@ -1,26 +1,35 @@
 # The idea is to just generate the wav file and the ground truth for speaker change detection.
 import re
+import os
+import logging
 
 import numpy as np
-import os
 import scipy
 import scipy.io.wavfile as wav
+from functional import seq
+from fn import _ as X, F
 
 import htkmfc as htk
 
+SAMPLE_RATE = 16000
+
 #### DO NOT CHANGE ####
 # base='/home/neerajs/work/NEW_REGIME/WAV/'
-base = '/home/siddharthm/'
+# base = '/home/siddharthm/'
 clean = 'clean_wav/'
 rev = 'rev_wav/'
 rev_noise = 'rev_noise_wav/'
 rev_inaud = 'rev_inaud_wav/'
-phndir = '/home/siddharthm/TIMIT/phones/'
+# phndir = '/home/siddharthm/TIMIT/phones/'
+phndir = '/Users/qianws/jupyterNotebooks/leap-scd/res/timit/gathered_phns/'
 choices = [clean, rev, rev_noise, rev_inaud]
 #### ------------- ####
 
-wavesavdir = '/home/siddharthm/scd/wavnew/train/'  # Save the generated wave file in this dir
-over_addr = '/home/siddharthm/scd/vad/10/train/'  # labels in the directory
+# wavesavdir = '/home/siddharthm/scd/wavnew/train/'  # Save the generated wave file in this dir
+wavesavdir = '/Users/qianws/jupyterNotebooks/leap-scd/res/wavnew/train/'  # Save the generated wave file in this dir
+# over_addr = '/home/siddharthm/scd/vad/10/train/'  # labels in the directory
+over_addr = '/Users/qianws/jupyterNotebooks/leap-scd/res/flabel/train/'  # labels in the directory
+
 common_save = 'train_combinations'
 ### SOME VARIABLE DEFINITIONS ###
 ratio_sc = 0.10
@@ -36,96 +45,87 @@ silence_samples = 0.00 * decision_samples  # The number of samples to be inserte
 # There are many ways in which we can generate speaker change files. Right now, we are generating by concatenating the two files.
 
 def data_saver(data):
-    os.chdir('/home/siddharthm/scd/scores')
-    f = open(common_save + '.list', 'a')
+    """ write (file1,file2) to common_save('train_combinations')
+    :param data:
+    :return:
+    """
+    # os.chdir('/home/siddharthm/scd/scores')
+    os.chdir('/Users/qianws/jupyterNotebooks/leap-scd/res/scores')
+    f = open(common_save + '.list', 'a+')
     f.write('\n')
     f.write(str(data))
     f.close()
 
 
 def gen_func(file1, file2, input_index):
-    print('Begin')
-    print((file1, file2, input_index))
+    """ merge 2 wav files, generate train data """
+    logging.info('---------- %s, %s, %s', file1, file2, input_index)
     data_saver(str(file1) + "," + str(file2))
-    ind1 = file1.index('_') + 1
-    ind2 = file2.index('_') + 1
-    file1_mod = file1[0:ind1] + file1[ind1 + 1:]
-    file2_mod = file2[:ind2] + file2[ind2 + 1:]
+
+    file1_mod = file1.replace('_SX', '_S').replace('_SI', '_S')
+    file2_mod = file1.replace('_SX', '_S').replace('_SI', '_S')
+
     # Fetching the base directory of the working files
-    wav_addr1 = base + choices[np.random.randint(1)]  # Keeping only the clean files
-    wav_addr2 = base + choices[np.random.randint(1)]  # Same as above
+    wav_addr1 = '/Users/qianws/jupyterNotebooks/leap-scd/res/timit/gathered_wavs/train/'
+    wav_addr2 = wav_addr1
+
     # reading the two wav files and overlapping them
     wav_file1_mod = wav_addr1 + file1_mod + '.wav'
     wav_file1 = wav_addr1 + file1 + '.wav'
     wav_file2_mod = wav_addr2 + file2_mod + '.wav'
     wav_file2 = wav_addr1 + file2 + '.wav'
-    # print(wav_file2)
-    phnFile1 = phndir + file1 + '.PHN'
-    phnFile2 = phndir + file2 + '.PHN'
+
+    phnFile1 = phndir + file1 + '.phn'
+    phnFile2 = phndir + file2 + '.phn'
 
     #### Check if the first line begins with 0 for h#
+    #### find silence parts of each file
+    REGEX_SIL = re.compile(r'.*h#|epi|sil')
 
-    nsFile1 = []
-    phnFile1Fid = open(phnFile1).readlines()
-    if int(phnFile1Fid[0].split(' ')[0]) != 0:
-        nsFile1.append([0, int(phnFile1Fid[0].split(' ')[1]) / 160])
-    for line in phnFile1Fid:
-        line = line.rstrip()
-        if re.search(('h#|epi|sil'), line):
-            nsFile1.append([int(line.split(' ')[0]) / 160, int(line.split(' ')[1]) / 160])
-    # print "Silence regions, file1: "
-    # print nsFile1
-    nsFile2 = []
-    phnFile2Fid = open(phnFile2).readlines()
-    if int(phnFile2Fid[0].split(' ')[0]) != 0:
-        nsFile2.append([0, int(phnFile2Fid[0].split(' ')[0]) / 160])
+    def find_sil_in_phn_file(phn_file_path):
+        with open(phn_file_path) as f:
+            lines = f.readlines()
+        res = (seq(lines)
+               .map(str.rstrip)
+               .filter(REGEX_SIL.match)
+               .map(lambda ln: tuple(int(s) // 160 for s in ln.split(' ')[:2]))
+               .to_list()
+               )
+        return res
 
-    for line in phnFile2Fid:
-        line = line.rstrip()
-        if re.search(('h#|epi|sil'), line):
-            nsFile2.append([int(line.split(' ')[0]) / 160, int(line.split(' ')[1]) / 160])
-    # print "Silence regions, File 2: "
-    # print nsFile2
-    # Wavfile.read returns the sampling rate and the read data. The sampling rate is assumed to be 16KHz for our purposes.
-    try:
-        [a1, a2] = wav.read(wav_file1)
-        a2 = np.reshape(a2, (1, a2.shape[0]))  # a2 is the actual data sample, reshaping it to (1,size)
-    except IOError:
-        [a1, a2] = wav.read(wav_file1_mod)
-        a2 = np.reshape(a2, (1, a2.shape[0]))  # a2 is the actual data sample, reshaping it to (1,size)
+    nsFile1, nsFile2 = seq([phnFile1, phnFile2]).map(find_sil_in_phn_file).to_list()
 
-    try:
-        [b1, b2] = wav.read(wav_file2)
-        b2 = np.reshape(b2, (1, b2.shape[0]))  # b2 is the sample, and reshaping it
-    except IOError:
-        [b1, b2] = wav.read(wav_file2_mod)
-        b2 = np.reshape(b2, (1, b2.shape[0]))  # b2 is the sample, and reshaping it
-
-    # Making them floats
-    a2 = a2.astype(float)
-    b2 = b2.astype(float)
+    # # Wavfile.read returns the sampling rate and the read data. The sampling rate is assumed to be 16KHz for our purposes.
+    f = F(wav.read) >> X[1] >> (lambda a: a.reshape(1, -1).astype(float))  # X[1], drop the sample_rate
+    a2, b2 = seq((wav_file1, wav_file2)).map(f)
 
     #### FRAME LEVEL MANIPULATIONS FOR CREATING OVERLAP LABELS ####
     nFrames1 = int(a2.shape[1] / 160)  # Number of frames that are possible from File one
     nFrames2 = int(b2.shape[1] / 160)  # Number of frames from File 2
 
-    # print nFrames1,nFrames2,overlap_frame,nsFile1,nsFile2
     stFrame2 = nFrames1
     totalFrame = nFrames1 + nFrames2 - 1
-    # print stFrame2,totalFrame,nsFile2[0][0]+stFrame2
 
+    # labeling where are the frames from: 1*nFrames1 ... 0*nFrames, except for the silence
     labelFrames1 = np.ones((totalFrame,), dtype=np.int)
-    for i in range(len(nsFile1)):
-        labelFrames1[nsFile1[i][0]:nsFile1[i][1]] = 0
+    # assign silence to 0
+    for s, e in nsFile1: labelFrames1[s:e] = 0
+    # for i in range(len(nsFile1)):
+    #     labelFrames1[nsFile1[i][0]:nsFile1[i][1]] = 0
     labelFrames1[nFrames1:] = 0
 
+    # 0*nFrames1 ... 2*nFrames2
     labelFrames2 = np.full((totalFrame,), 2,
                            dtype=np.int)  # Giving different identity to second speaker file[By using 2]
-    labelFrames2[:stFrame2] = 0
-    for i in range(len(nsFile2)):
-        labelFrames2[nsFile2[i][0] + stFrame2:nsFile2[i][1] + stFrame2] = 0
+    labelFrames2[:stFrame2] = 0  # setting the left part
+    for s, e in nsFile2:
+        labelFrames2[s + stFrame2:e + stFrame2] = 0
+
     ### GENERATING THE FINAL LABEL FILE ###
-    # print type(labelFrames1)
+    # terribly written, do not try to understand every line
+    # just know that
+    # * `out` is the concated wav data with silence trimed
+    # * `flabels` is the 0/1/2 labels per 600ms, written in HTK format
     lastindex = np.where(labelFrames1 == 1)[0][-1]
     firstindex = np.where(labelFrames2 == 2)[0][0]
     silence_part = np.zeros((int(silence_samples / 160),), dtype=np.int)
@@ -136,7 +136,6 @@ def gen_func(file1, file2, input_index):
     labelpart2 = np.hstack((labelFrames2[firstindex:]))  # silence part was not extraneous, was coming twice
     labelFrames = np.hstack((labelpart1, labelpart2))
     # print "Length of the labelFRames vector: ",labelFrames.shape
-    print(labelFrames)
     start = 0
     iterator = 0
     skip_entries = int(decision_samples / 160)
@@ -171,43 +170,27 @@ def gen_func(file1, file2, input_index):
         else:
             dec = 0
             flag += 1
-            # Update section
-            # print "Decision Taken: ",dec
-            # print "\n"
-            # print count_zero,count_one,count_two,dec
-            # if dec==2:
-            # count_two+=1
-            # elif dec==1:
-            # count+=1
-            # else:
-            # flag+=1
         flabels.append(dec)
-        # print "Flag: ", flag,"Count: ",count
         iterator += 1
         start += 1
         end = skip_entries + start
 
-    # print count,flag,count_TWO
-    # print iterator
     ### Setting the labels and the output ###
     out = out.astype(np.int16)
     out = np.reshape(out,
                      (out.shape[0], 1))  # Reshaping it to form the vector which is required to be written in wav file
 
-    # print "The shape of out vector: ",out.shape
     flabels = np.array(labelFrames)
-    # print flabels.shape
-    # print type(flabels)
     flabels = np.reshape(flabels, (1, flabels.shape[0]))
     ### SAVING THE STUFF SECTION ###
-    # print "Labels: ",flabels
-    scipy.io.wavfile.write(wavesavdir + file1 + '-' + file2 + '-' + str(input_index) + '.wav', a1, out)
+    scipy.io.wavfile.write(wavesavdir + file1 + '-' + file2 + '-' + str(input_index) + '.wav', SAMPLE_RATE, out)
     writer = htk.open(over_addr + file1 + '-' + file2 + '-' + str(input_index) + '.htk', mode='w',
                       veclen=max(flabels.shape))
     writer.writeall(flabels)
-    ### --------- ###
+
 
 ### TRY CALLS[Actual use with wrapper] ###
-# gen_func('FCJF0_SX397','MJMM0_SX445',15775)
+# gen_func('FCJF0_SX397', 'MJMM0_SX445', 15775)
 # gen_func('FTBW0_X85','FTLG0_I1743',2)
+pass
 ### ------- ###
